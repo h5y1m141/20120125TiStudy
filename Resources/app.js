f8868b6a9fb153c9b92446856fa0e98a17561c29
hiroshi oyamada <h5y1m141+github@gmail.com>
2012-01-25 18:47:38 +0900

    不要なコードをふくんでマージしてしまったのでそれを除去;

--- Resources/app.js
+++ Resources/app.js
@@ -38,31 +38,3 @@ var mapView = Titanium.Map.createView($$.mapView);
 win1.add(mapView);
 win1.add(tableView);
 win1.open();
-
-<<<<<<< HEAD
-//googleFusionTables.select('GET',param1);
-
-
-// var testData = {
-//   "table": {
-//     "cols":["shop_name","comment","user_id","post_date"],
-//     "rows":[
-//      ["地ビールダイニング神楽坂ラ・カシェット","南信州のアンバーエールが美味しかった","h5y1m141","2011/12/16"],
-//      ["地ビールダイニング神楽坂ラ・カシェット","桃のビールとマスカットのビール飲んだけどマスカットのほうが好みだった","tnakamaw","12/16/11"]
-//    ]
-//   }
-// };
-// var obj = ui.util(eval(testData));
-// Ti.API.info('after:'+obj[0].comment);
-
-var tableid   =2415682;
-var tableName ='craftBeer';
-var param2 ={
-  url:"https://www.google.com/fusiontables/api/query?sql=",
-  sqlStatement:"SELECT * FROM " + tableid,
-  callback:"ui.util"
-};
-
-googleFusionTables.select('GET',param2);
-=======
->>>>>>> MapPlusAddressBook
