5af6c3ef594104c67ac257cff4db7685328d34ac
hiroshi oyamada <h5y1m141+github@gmail.com>
2012-01-31 17:26:26 +0900

    ひとまずtableview→mapviewへの遷移成功

--- Resources/ui/styles.js
+++ Resources/ui/styles.js
@@ -14,20 +14,17 @@ var prop = {
   mapView:{
     width: 320,
     height:640,
-    top:200,
+    top:0,
     left:0,
     mapType: Titanium.Map.STANDARD_TYPE,
-    region: {
-      latitude:35.676564,
-      longitude:139.765076,
-      // 1.0から0.001の間で縮尺尺度を示している。
-      // 数値が大きい方が広域な地図になる。donayamaさんの書籍P.179の解説がわかりやすい
-      latitudeDelta:0.01,
-      longitudeDelta:0.01
-    },
     animate:true,
     regionFit:true,
     userLocation:true
+  },
+  mapWin:{
+    modal:true,
+    title:'地図の表示',
+    backgroundColor:theme.backgroundColor
   },
   mapAnnotation:{
     pincolor:Titanium.Map.ANNOTATION_GREEN,
@@ -63,7 +60,11 @@ var prop = {
     top:5,
     width:60,
     height:60
+  },
+  closeBtn:{
+    title:'閉じる'
   }
+
 };
 
 var exports = {
--- Resources/ui/ui.js
+++ Resources/ui/ui.js
@@ -8,11 +8,24 @@ var exports = {
         e.rowData.data.latitude,e.rowData.data.longitudeにて
         緯度経度情報を取得可能
       */
+
       var pin = Titanium.Map.createAnnotation($$.mapAnnotation);
-      pin.latitude = e.rowData.data.latitude,
-      pin.longitude = e.rowData.data.longitude,
-      pin.title = e.rowData.data.name,
-      setMapView(pin,e.rowData.data.latitude,e.rowData.data.longitude);
+      var mapView = Titanium.Map.createView($$.mapView);
+      mapView.addAnnotations([pin]);
+      mapView.region = {
+        latitude:e.rowData.data.latitude,
+        longitude:e.rowData.data.longitude,
+        latitudeDelta:0.01,
+        longitudeDelta:0.01
+      };
+      var mapWin = Ti.UI.createWindow($$.mapWin);
+      var closeBtn = Titanium.UI.createButton($$.closeBtn);
+      closeBtn.addEventListener('click',function(){
+        mapWin.close();
+      });
+      mapWin.leftNavButton = closeBtn;
+      mapWin.add(mapView);
+      mapWin.open();
 
     });
     var shopName = Ti.UI.createLabel($$.shopName);
@@ -41,6 +54,7 @@ var exports = {
 
 // private method
 function setMapView(pin,latitude,longitude){
+  var mapView = Titanium.Map.createView($$.mapView);
   mapView.addAnnotations([pin]);
   mapView.setLocation({
     latitude :latitude,
@@ -48,4 +62,9 @@ function setMapView(pin,latitude,longitude){
     latitudeDelta:0.01,
     longitudeDelta:0.01
   });
+  return mapView;
+}
+
+function createCloseBtn(){
+
 }
